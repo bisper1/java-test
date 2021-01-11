7257eb73f3ebd725bfd358772b28452536c1fc3b
bisper1 <bisper@naver.com>
2021-01-11 17:17:40 +0900

    포트폴리오 문제 5 Game 클래스 입력 완료.

--- src/PortFolioQuestion5/Game.java
+++ src/PortFolioQuestion5/Game.java
@@ -19,4 +19,55 @@ public class Game {
 		m[0] = new Bear(0, 0, 1);
 		m[1] = new Fish(5, 5, 2);
 	}
+	
+	public void run() {
+		System.out.println("** Bear의 Fish 먹기 게임을 시작합니다.**");
+		
+		update();
+		draw();
+		
+		while(!doesEnd()) {
+			clear();
+			
+			for (int i = 0; i < m.length; i++) {
+				m[i].move();
+			}
+			
+			update();
+			draw();
+		}
+		
+		System.out.println("Bear Wins!!");;
+	}
+	
+	private void update() {
+		for (int i = m.length - 1; i >= 0; i--) {
+			map[m[i].getY()][m[i].getX()] = m[i].getShape();
+		}
+	}
+	
+	private void clear() {
+		for (int i = 0; i < m.length; i++) {
+			map[m[i].getY()][m[i].getX()] = m[i].getShape();
+		}
+	}
+	
+	private void draw() {
+		System.out.println();
+		
+		for (int i = 0; i < MAX_Y; i++) {
+			for (int j = 0; j < MAX_X; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	private boolean doesEnd() {
+		if (m[0].collide(m[1])) {
+			return true;
+		}
+		
+		return false;
+	}
 }
